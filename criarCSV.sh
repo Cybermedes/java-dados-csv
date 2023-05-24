1cd3addc8463f23bb7f27a793b9d2c57241882c9
Lucas Paiva <lpaivamatiazzi@gmail.com>
2023-05-24 10:32:07 -0300

    Adiciona comentarios ao script

--- criarCSV.sh
+++ criarCSV.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# Checa se o arquivo csv já existe e não cria um novo caso contrário
+# Se o arquivo existir e o script rodar novamente, novos dados serão adicionados
 arquivo=carros.csv
 if [[ -f "$arquivo" ]]; then
     echo "Arquivo $arquivo já existe"
@@ -9,11 +11,13 @@ else
     echo "Ano,Modelo,Cor,Novo">$arquivo
 fi
 
+# Declara as arrays que irão conter os dados que irão preencher as tabelas
 declare -a modelos=()
 declare -a cores=("amarelo" "vermelho" "azul" "preto" \
 "prata" "branco")
 declare -a status=("novo" "usado")
 
+# Loop para criar linhas contendo dados aleatorios
 echo "Criando tabela"
 for (( i = 1; i <= 50; i++ )); do
 ano=$(( RANDOM % 12 + 2011))
@@ -23,4 +27,5 @@ statusVar=$(( RANDOM % 2 ))
 echo "">>$arquivo
 done
 
+# Mensagem para indicar que o script finalizou
 echo "Tabela csv criada"
