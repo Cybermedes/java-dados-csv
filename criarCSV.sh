6f2d9164c8fb0778a6c47da291dc21f0577f6b92
Lucas Paiva <lpaivamatiazzi@gmail.com>
2023-05-24 10:25:27 -0300

    Declara arrays para criar dados e modifica loop

--- criarCSV.sh
+++ criarCSV.sh
@@ -9,11 +9,18 @@ else
     echo "Ano,Modelo,Cor,Novo">$arquivo
 fi
 
+declare -a modelos=()
+declare -a cores=("amarelo" "vermelho" "azul" "preto" \
+"prata" "branco")
+declare -a status=("novo" "usado")
+
 echo "Criando tabela"
-for (( i = 1; i <= 30; i++ )); do
-ano=$(( RANDOM % 20 + 2002))
+for (( i = 1; i <= 50; i++ )); do
+ano=$(( RANDOM % 12 + 2011))
 modeloVar=$(( RANDOM % 6 ))
 corVar=$(( RANDOM % 6 ))
 statusVar=$(( RANDOM % 2 ))
 echo "">>$arquivo
 done
+
+echo "Tabela csv criada"
