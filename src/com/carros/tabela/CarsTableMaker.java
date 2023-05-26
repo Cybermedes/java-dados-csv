7e0890040a40bcd21881ec060f159ce168137334
Lucas Paiva <lpaivamatiazzi@gmail.com>
2023-05-26 19:59:35 -0300

    Metodo para checar/criar arquivo csv

--- src/com/carros/tabela/CarsTableMaker.java
+++ src/com/carros/tabela/CarsTableMaker.java
@@ -1,6 +1,7 @@
 package com.carros.tabela;
 
 import java.io.File;
+import java.io.IOException;
 
 public class CarsTableMaker {
 
@@ -13,12 +14,26 @@ public class CarsTableMaker {
 
         String tabelaNome = "carros.csv";
         File tabela = new File(tabelaNome);
+
+        if (tabela.exists()) {
+            System.out.println("Arquivo existe e está na pasta"
+                    + tabela.getAbsolutePath());
+        } else {
+            System.out.println("Arquivo não existe. Criando um novo...");
+            try {
+                tabela.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            preencherTabela(tabela);
+        }
     }
 
     /*
     Metodo para popular a tabela carros.csv com dados aleatórios
      */
-    public static void preencherTabela() {
+    public static void preencherTabela(File tabela) {
+
         int ano, modeloVar, corVar, statusVar;
     }
 }
