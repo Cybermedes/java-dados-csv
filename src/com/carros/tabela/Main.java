d514f15c2ad87312dfcb521479d27a038644bc96
Lucas Paiva <lpaivamatiazzi@gmail.com>
2023-05-30 21:33:17 -0300

    Cria classe principal e metodo main

---
+++ src/com/carros/tabela/Main.java
@@ -0,0 +1,10 @@
+package com.carros.tabela;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        CarsTableMaker tabelaCarros = new CarsTableMaker();
+        tabelaCarros.criarTabela();
+    }
+}
