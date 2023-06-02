e017afe097150fb81dce5af75a399f9d29eca58d
Lucas Paiva <lpaivamatiazzi@gmail.com>
2023-06-02 10:23:10 -0300

    Cria a liste de objetos carros e os imprime

--- src/com/carros/tabela/Main.java
+++ src/com/carros/tabela/Main.java
@@ -1,10 +1,21 @@
 package com.carros.tabela;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
 
     public static void main(String[] args) {
 
         CarsTableMaker tabelaCarros = new CarsTableMaker();
         tabelaCarros.criarTabela();
+
+        //Imrpime todos os objetos
+        List<String> lines = TextLoader.getLines("carros.csv");
+        List<Carro> veiculos = lines.stream()
+                .map((line) -> CarroRecordUtil.parseCarro(line))
+                .collect(Collectors.toList());
+
+        veiculos.forEach((veiculo) -> System.out.println(veiculo));
     }
 }
