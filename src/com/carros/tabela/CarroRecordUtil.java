649de88d8b87132d223eab9118e6d2758c42c543
mateus <mateus.matiazzi@ab-inbev.com>
2023-06-04 19:01:15 -0300

    My recommendation for this one it's just to create constants for each index number. In this case this don't make any changes at all, but in the future it's possible to be needed to add a new attribute in the Carros class, this way it's way easier to change the indexes in the class without problem.

--- src/com/carros/tabela/CarroRecordUtil.java
+++ src/com/carros/tabela/CarroRecordUtil.java
@@ -5,15 +5,22 @@ import java.util.Arrays;
 import java.util.List;
 
 public class CarroRecordUtil {
+
+    private static final int INDEX_FABRICANTE = 0;
+    private static final int INDEX_MODELO = 1;
+    private static final int INDEX_ANO = 2;
+    private static final int INDEX_COR = 3;
+    private static final int INDEX_CONDICOES = 4;
+
     public static Carro parseCarro(String dataString) {
         List<String> campos = new ArrayList<>(Arrays.asList(dataString.split(",")));
 
         try {
-            String fabricante = campos.get(0);
-            String modelo = campos.get(1).trim();
-            Integer ano = Integer.parseInt(campos.get(2).trim());
-            String cor = campos.get(3).trim();
-            String condicoes = campos.get(4).trim();
+            String fabricante = campos.get(INDEX_FABRICANTE);
+            String modelo = campos.get(INDEX_MODELO).trim();
+            Integer ano = Integer.parseInt(campos.get(INDEX_ANO).trim());
+            String cor = campos.get(INDEX_COR).trim();
+            String condicoes = campos.get(INDEX_CONDICOES).trim();
 
             return new Carro(fabricante, modelo, ano, cor, condicoes);
         } catch(Exception e) {
